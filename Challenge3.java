5b03f26ee4f2b4fb97827c8b10a57a3829ff8532
ZeynepM-Yalcin <113379342+ZeynepM-Yalcin@users.noreply.github.com>
2022-11-09 18:49:32 +0000

    Update Challenge3.java

--- Challenge3.java
+++ Challenge3.java
@@ -1,4 +1,8 @@
-public class Challenge2 {
+public class Challenge3 {
+    public static void main(String args[]){
+        int number = Integer.parseInt(args[0]);
+        for(int i = 0;i <= 10;i++){
+            System.out.println(number*i);
 
-
-}
+        }
+    }
